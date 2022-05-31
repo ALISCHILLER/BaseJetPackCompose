f28bf1cd0547e0f7dd2e4e6cb2f9e94c1137d13d
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-05-31 18:56:23 +0430

    compose calculator

--- app/src/main/java/com/msa/basejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/msa/basejetpackcompose/MainActivity.kt
@@ -3,13 +3,21 @@ package com.msa.basejetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.msa.basejetpackcompose.ui.theme.BaseJetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +39,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        fontSize =32.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(Color.Yellow)
+            .border(2.dp, Color.Green)
+            .padding(10.dp)
+
+        )
+
 }
 
 @Preview(showBackground = true)
