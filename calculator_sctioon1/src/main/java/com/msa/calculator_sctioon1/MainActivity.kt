07878e429242a2b8a7ae7c0bed9870668fb9dc79
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-06-01 18:53:52 +0430

    calculator InputText

--- calculator_sctioon1/src/main/java/com/msa/calculator_sctioon1/MainActivity.kt
+++ calculator_sctioon1/src/main/java/com/msa/calculator_sctioon1/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,21 +16,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.msa.calculator_sctioon1.components.InputField
 import com.msa.calculator_sctioon1.ui.theme.BaseJetPackComposeTheme
-
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-               TopHeader()
+               //TopHeader()
+                MainContent()
             }
         }
     }
@@ -61,12 +66,18 @@ fun TopHeader(perPerson:Double=555.0){
     }
 }
 
+@ExperimentalComposeUiApi
 @Preview
 @Composable
 fun MainContent(){
     val totalBillState= remember {
         mutableStateOf("")
     }
+    val validState= remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+
+    }
+    val keyboardController=LocalSoftwareKeyboardController.current
    Surface(
         modifier = Modifier
             .padding(1.dp)
@@ -79,7 +90,12 @@ fun MainContent(){
                     valueState =totalBillState ,
                     labelId = "Enter Bill",
                     enabled = true,
-                    isSingleLine =true )
+                    isSingleLine =true,
+                    onAction = KeyboardActions{
+                        if (!validState)return@KeyboardActions
+                        //Todo - onvaluechanged
+                        keyboardController?.hide()
+                    })
 
             }
     }
